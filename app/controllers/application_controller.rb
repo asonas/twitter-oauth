3abba5a455028847ab1cbed6e0a2453c3844b148
asonas <hzw1258@gmail.com>
2011-06-21 02:33:52 +0900

    create index_controller, modified Gemfile and routes.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  protected
+  def self.consumer
+    OAuth::Consumer.new(
+      'consumer', #consumer key
+      'consumer secret', #secret key
+      { site: "http://twitter.com" }
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 TwitterOauth::Application.routes.draw do
+  #get "index/index"
+  #get "index/oauth"
+  #get "index/callback"
+
+  root to: "index#index", as: :index
+  get "/oauth" => "index#oauth", as: :oauth
+  get "/callback" => "index#callback", as: :callback
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
